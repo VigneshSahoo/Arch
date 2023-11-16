0e3b171841d4f844147148c0409b00b1e02ff2d9
PythonTrainerVignesh <vigneshkumar9797@gmail.com>
2023-11-16 09:04:47 +0530

    Created an app called CRUD

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+# This is a table in MySQL database.
+# models.Model - Model stands for a table in the DB
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+    class Meta:
+        db_table = 'products'
--- CRUD/views.py
+++ CRUD/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+# Method 1: Directly importing using the model name
+from .models import Products
+# Method 2: Importing via models file
+from . import models
 
 
 # Create your views here.
@@ -18,5 +22,11 @@ def delete(request):
 
 
 def read(request):
-    context = {}
+    # Method 2: calling the data from models file
+    data = models.Products.objects.all()
+    # Method 1: calling the products table directly from the models file
+    products_data = Products.objects.all()
+    context = {
+        'html_data': products_data,
+    }
     return render(request, 'read.html', context)
